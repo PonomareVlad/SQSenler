9ff2721da4e9210b0bb15449f941e84998157f33
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2026-01-21 18:24:57 +0100

    Refactor POST handler to extract `utm_campaign` and move `utms` logic to a separate module

--- api/utms.mjs
+++ api/utms.mjs
@@ -1,6 +1,7 @@
 export const POST = async request => {
     const data = await request.json()
     console.log(data)
-    const {object: {utms: {utm_campaign} = {}} = {}} = data
-    return new Response(utm_campaign)
+    const {object: {utms} = {}} = data
+    console.log(utms)
+    return Response.json(utms)
 }
