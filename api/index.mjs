4bfb55477ffcfe58ba30eaa252f6ac998f62d0e6
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2026-01-21 17:52:23 +0100

    Refactor POST handler to handle optional `utms` field safely

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,4 @@
 export const POST = async request => {
-    console.log(request.body)
-    const {object: {utms}} = request.body
+    const {object: {utms} = {}} = await request.body
     return Response.json(await request.json({vars: Object.entries(utms).map(([n, v]) => ({n, v}))}))
 }
