b60cd3fe334b04e1caacd2e1717d9f6b5892c2b6
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2026-01-21 18:05:01 +0100

    Add log for UTM parameters in POST handler for debugging

--- api/index.mjs
+++ api/index.mjs
@@ -2,5 +2,6 @@ export const POST = async request => {
     const data = await request.json()
     console.log(data)
     const {object: {utms} = {}} = data
+    console.log(utms)
     return Response.json({vars: Object.entries(utms).map(([n, v]) => ({n, v}))})
 }
