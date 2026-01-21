484a68910e4eff847ea4335cf29b61c2fb9ea288
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2026-01-21 17:55:02 +0100

    Remove unnecessary console log from POST handler

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,4 @@
 export const POST = async request => {
-    console.log(request)
     const {object: {utms} = {}} = await request.json()
     return Response.json(await request.json({vars: Object.entries(utms).map(([n, v]) => ({n, v}))}))
 }
