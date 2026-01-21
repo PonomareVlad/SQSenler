d624558ea3044f7c757eb69ed0a55c466fd6622a
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2026-01-21 18:02:00 +0100

    Simplify request body parsing in POST handler

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,6 @@
 export const POST = async request => {
-    const {object: {utms} = {}} = await request.json()
-    console.log(utms)
+    const data = await request.json()
+    console.log(data)
+    const {object: {utms} = {}} = data
     return Response.json({vars: Object.entries(utms).map(([n, v]) => ({n, v}))})
 }
